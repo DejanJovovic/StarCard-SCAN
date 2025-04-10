ed10875ecbebd88675e1c55d0d8eac764158e3b4
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2025-04-10 12:45:10 +0200

    nativewind init, assets added, started with the app

--- StarCard-Scan/app/index.tsx
+++ StarCard-Scan/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="justify-center items-center mt-10">
+      <Text className="text-black text-2xl font-bold">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
